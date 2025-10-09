5d694256fba6cfd4f05df7ad14db2b892383d76a
jrbrinkgreve <j.brinkgreve@icloud.com>
2025-10-09 15:50:13 +0200

    lowered number of iterations for slowpoke laptops

--- main.m
+++ main.m
@@ -32,7 +32,7 @@ delta = 1.5;     %stepsize: between 0 and 2 should work
 eps = 1e-10;    %fro norm tolerance: past this point break loop
 tau = 0.2;     %threshold step size
 l = 1;         %not in use currently, should be related to size of svd later for speedup
-kmax = 2000;    %max iterations, takes about 78 seconds for 1000 iterations (parallel channels)
+kmax = 1000;    %max iterations, takes about 78 seconds for 1000 iterations (parallel channels)
 k0 = 1;        %initial guess magnitude
 opts_matrix = [delta eps tau l kmax k0]; %input options vector
 
@@ -55,6 +55,10 @@ legend("Red channel", "Green channel", "Blue channel")
 
 %note: its kind of funny that the blue channel is worse: there are almost
 %no blue pixels to reconstruct so the relative error on blue is also larger
+
+
+
+
 
 %tensor completion algorithm here
 opts_tensor = [];
